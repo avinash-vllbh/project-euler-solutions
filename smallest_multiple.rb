40b61cf813ca10404b9dfd00ed60c3c7a9c74ef5
avinash vallabhaneni <avinash.vllbh@gmail.com>
2014-08-28 05:52:30 -0700

    removed the method with high time complexity and added complexity orders to existing methods

--- smallest_multiple.rb
+++ smallest_multiple.rb
@@ -5,37 +5,21 @@ require 'pry'
 require 'benchmark'
 include Math
 class SmallestMultiple
-  attr_accessor :primes, :composites, :euler_primes
+  attr_accessor :euler_primes
 
   def initialize
-    self.primes = [3,2]
     self.euler_primes = [3,2]
-    self.composites = []
   end
 
-  def ruby_way(input)
+  # Inbuild ruby method uses LCM(a,b) = a*b/GCD(a,b) to find the smallest multiple quicker
+  # Time complexity - O(n)
+  # Space complexity - O(n)
+  def get_smallest_multiple(input)
     (1..input).reduce(:lcm)
   end
 
-  def get_smallest_multiple(input)
-    primes_composites(input)
-    is_smallest_multiple = false
-    counter = 2
-    smallest_multiple = 0
-    loop do
-      smallest_multiple = input*counter
-      if(is_divisible_primes(smallest_multiple))
-        if(is_divisible_composites(smallest_multiple))
-          is_smallest_multiple = true
-          break
-        end
-      end
-      counter += 1
-      break if is_smallest_multiple
-    end
-    # p smallest_multiple
-  end
-
+  # Time complexity - O(n) [although actual time taken is larger than the above method]
+  # Space complexity - O(n)
   def euler_approach_sm(input)
     fill_primes(input)
     limit = Math.sqrt(input)
@@ -53,41 +37,6 @@ class SmallestMultiple
 
   private
 
-  def is_divisible_primes(number)
-    primes.each do |prime|
-      unless number%prime == 0
-        return false # If not divisible by even a single prime then break
-      end
-    end
-    return true
-  end
-
-  def is_divisible_composites(number)
-    composites.each do |composite|
-      unless number%composite == 0
-        return false # If not divisible by even a single prime then break
-      end
-    end
-    return true
-  end
-  # To make the primes array between 2 and input
-  def primes_composites(input)
-    for i in 4..input
-      is_prime = true
-      primes.each do |prime|
-        if i%prime == 0
-          is_prime = false
-          break
-        end
-      end
-      if is_prime
-        self.primes.unshift(i)
-      else
-        self.composites.unshift(i)
-      end
-    end
-  end
-
   def fill_primes(input)
     for i in 4..input
       is_prime = true
@@ -98,26 +47,25 @@ class SmallestMultiple
         end
       end
       if is_prime
-        self.euler_primes.unshift(i)
+        self.euler_primes.push(i)
       end
     end
   end
 end
 
-dividend = SmallestMultiple.new
-iterations = 10
+lcm = SmallestMultiple.new
+iterations = 10000
+
+# according to Benchmark results using the ruby method for finding LCM is much faster that the one given by Project Euler
 Benchmark.bm(40) do |bm|
   bm.report('ruby way of finding LCM') do
     iterations.times do
-      dividend.ruby_way(20)
+      lcm.get_smallest_multiple(20)
     end
   end
   bm.report('Project Euler way of finding LCM ') do
     iterations.times do
-      dividend.euler_approach_sm(20)
+      lcm.euler_approach_sm(20)
     end
-  end
-  bm.report('My crappy way of doing LCM :( ') do
-    dividend.get_smallest_multiple(20)
   end
 end
